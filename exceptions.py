f2c5f136af4837abae6d172838d4ae94dd5978d7
Taser <81585596+TaserTheFox@users.noreply.github.com>
2023-05-09 16:31:30 -0700

    HOLY SHITTTT I GOT IT TO WORKKK!!! mostly

---
+++ exceptions.py
@@ -0,0 +1,54 @@
+class _NotImplemented(Exception):
+    def __str__(self):
+        return 'Not implemented'
+
+class _noEnd(Exception):
+    def __str__(self):
+        return '\033[38;5;14mUNENDING PAIN AND SUFFERING'
+
+class _undeclared_var(Exception):
+    def __init__(self, var):
+        super().__init__()
+        self.var = var
+
+    def __str__(self):
+        return f"\033[38;5;14m{self.var} WASN'T NOTICED"
+
+class _caperror(Exception):
+    def __str__(self):
+        return "\033[38;5;14mSYNTAX ERROR"
+
+class _jump_error(Exception):
+      def __init__(self, cond,symbol_table):
+        self.cond=cond
+        self.symbol_table=symbol_table
+
+      def __str__(self):
+        return f"\033[38;5;14mSOMETHING WENT WRONG EVALUATING: {self.cond}\nHERE IS THE CURRENT SYMBOL TABLE:\n {self.symbol_table}"
+
+class _noLabel(Exception):
+    def __init__(self, label):
+        self.label=label
+      
+    def __str__(self):
+        return f"\033[38;5;14mUNKNOWN MARKING: {self.label}"
+
+class _noStart(Exception):
+    def __str__(self):
+        return "\033[38;5;14mWHEN DID IT EVEN START"
+
+class _noBoop(Exception):
+    def __str__(self):
+        return "\033[38;5;14mFAILED TO BOOP THE USER"
+
+class _tooManyBoop(Exception):
+    def __str__(self):
+        return "\033[38;5;14mTOO MANY BOOPS!"
+
+class _castingFail(Exception):
+    def __str__(self):
+        return "\033[38;5;14mTRANSFORMATION FAILED"
+
+class _unmatchedComment(Exception):
+    def __str__(self):
+        return "\033[38;5;14mWHICH IS BETTER? PAWS OR MAWS?"
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from exceptions import _noEnd,_undeclared_var,_caperror,_jump_error,_noLabel,_noStart,_noBoop,_tooManyBoop,_castingFail,_unmatchedComment
 class EsoFurCompiler:
     def __init__(self):
         self.symbol_table = {}
         self._in_comment = False
-
+        #self._in_loop==False
     def compile(self, code):
         lines = code.split('\n')
         i = 0
         built=False
+        if lines.count("Maws") != lines.count("Paws"):
+            raise self._unmatchedComment()
         while i < len(lines):
             line = lines[i].strip()
 
@@ -14,30 +17,59 @@ class EsoFurCompiler:
             if not line:
                 i+=1
                 continue
-
-            if "OwO What's This?" in line :
-              built=True
-              i+=1
-              continue
+            
+            #check if program should run at all
+            if line == "OwO What's This?":
+                built=True
+                i+=1
+                continue
 
             elif not built:
-              exit(0)
+                exit(0)
 
+            #end of program
             if line=="QwQ":
-              exit("finished")
+                exit("\033[38;5;15mfinished")
 
             if "QwQ" not in lines:
-              exit(1)
+                raise _noEnd()
 
             # Comment handling
             if line.startswith('Muzzles'):
                 i+=1
                 continue
+            
+            if line == "Maws":
+                if self._in_comment == True:
+                    raise _unmatchedComment()
+                self._in_comment = True
+                i+=1
+                continue
+            
+            if line == "Paws":
+                if self._in_comment == False:
+                    raise _unmatchedComment()
+                self._in_comment = False
+                i+=1
+                continue
+
+            if self._in_comment == True:
+                i+=1
+                continue
+            
+            #marking a line
+            if line.startswith("Marks"):
+                i+=1
+                continue
+
+            #syntax check
+            if line.istitle()==False:
+                raise _caperror()
 
             # Variable declaration
             if line.startswith('Notices Your'):
                 var_name = line.split(' ')[2]
-                self.symbol_table[var_name] = None
+                self.symbol_table.update({var_name:None})
                 i+=1
                 continue
 
@@ -45,20 +77,33 @@ class EsoFurCompiler:
             if 'Pounces On' in line:
                 value, var_name = line.split('Pounces On')
                 var_name = var_name.strip()
-                value = self._parse_value(value.strip())
+                if var_name not in self.symbol_table:
+                    raise _undeclared_var(var_name)
+                value = self._assign(value.strip())
                 self.symbol_table[var_name] = value
                 i+=1
                 continue
 
             # Jumps
             if 'Nuzzles' in line:
-                condition, label = line.split('Nuzzles')
-                condition = self._parse_value(condition.strip())  # evaluate condition
-                label = label.strip()
-                if bool(condition):  # use bool() to convert the parsed condition to aboolean
-                  i = int(self._find_label_index(lines, label))
+                if line.startswith("Nuzzles")==False:
+                    condition, label = line.split('Nuzzles')
+                    condition = self._parse_value(condition.strip())  # evaluate condition
+                    #label = label.strip()
+                    label = self._assign(label.strip())
+                    if bool(condition):  # use bool() to convert the parsed condition to a boolean
+                        if label.isdigit():
+                            i+=label
+                            continue
+                        i = int(self._find_label_index(lines, label))
+                    else:
+                        i+=1
                 else:
-                 i+=1
+                    label =  self._assign(line.split()[1].strip())
+                    if type(label)==int:
+                        i+=label
+                        continue
+                    i = int(self._find_label_index(lines, label))
                 continue
 
 
@@ -67,35 +112,44 @@ class EsoFurCompiler:
                 i+=1
                 continue
 
-            if line.startswith('*Stops Roleplaying'):
-                condition = line.split(' ')[-1]
+            if line.startswith('*Stops Roleplaying Because Of') and line.endswith('*'):
+                condition = line.split(' ')[-1][:-1].strip()
                 condition = self._parse_value(condition)
-                if condition > 0:
+                if bool(condition):
                     i = self._find_loop_start_index(lines, i)
                 else:
-                  i+=1
+                    i+=1
                 continue
 
             # Print
             if line.startswith('Howl'):
-                var_name = line.split(' ')[-1]
-                value = self._parse_value(var_name)
+                var_name = line.split(' ')[1]
+                value = self._assign(var_name.strip())
                 print(value)
                 i+=1
                 continue
 
               # Ask user for input
             if line.startswith('Boop The User For'):
-              var_name = line.split(' ')[-1]
-              value = input()
-              self.symbol_table[var_name] = value
-              i+=1
-              continue
+                text=line.split()
+                var_name = text[4]
+                prompt=':'
+                if len(text) == 7:
+                    if text[5]!='With':
+                        raise _noBoop()
+                    prompt=self._assign(text[6])+':'
+                if len(text) > 7:
+                    raise _tooManyBoop()
+                value = input(prompt)
+                value = self._assign(value)
+                self.symbol_table[var_name] = value
+                i+=1
+                continue
 
 
             # Type casting
             if 'Transforms Into' in line:
-                var_name, type_name = line.split('Transforms Into ')
+                var_name, type_name = line.split('Transforms Into')
                 var_name = var_name.strip()
                 type_name = type_name.strip()
                 value = self._parse_value(var_name)
@@ -108,19 +162,33 @@ class EsoFurCompiler:
         #    print("Error: Unclosed multiline comment!")
 
     def _find_label_index(self, lines, label):
-        if label.isdigit():
-          return label
         for i, line in enumerate(lines):
             if line.startswith('Marks') and line.split(' ')[1] == label:
                 return i
-        print(f"Error: Label not found: {label}")
+        raise _noLabel(label)
+        #print(f"Error: Label not found: {label}")
 
     def _find_loop_start_index(self, lines, end_index):
+        loop_count=0
         for i in range(end_index - 1, -1, -1):
+            if lines[i].startswith('*Stops Roleplaying Because Of') and lines[i].endswith('*'):
+                loop_count+=1
             if lines[i] == '*Starts Roleplaying*':
-                return i
-        print("Error: Loop start not found")
+                if loop_count == 0:
+                    return i
+                else:
+                    loop_count-=1
+        raise _noStart()
+        #print("Error: Loop start not found")
 
+    def _assign(self, value_str):
+        if value_str.isdigit():
+            return int(value_str)
+        elif value_str in self.symbol_table:
+            return self.symbol_table[value_str]
+        else:
+            return value_str
+        
     def _parse_value(self, value_str):
         if value_str.isdigit():
             return int(value_str)
@@ -129,9 +197,8 @@ class EsoFurCompiler:
         try:
             return eval(value_str, {}, self.symbol_table)
         except:
-            print(f"Error: Invalid expression: {value_str}")
-
-
+            raise _jump_error()
+        
     def _cast_value(self, value, type_name):
         if type_name == 'Int':
             return int(value)
@@ -143,27 +210,13 @@ class EsoFurCompiler:
             return list(value)
         if type_name == 'Furpile':
             return set(value)
-        print(f"Error: Invalid type: {type_name}")
-
-
-
-
-code='''
-OwO What's This?
-Notices Your Input
-Howl "INPUT"
-Boop The User For Input
-Input Transforms Into Int
-Input Nuzzles 8
-Howl 0
-Nuzzles 7
-Howl "LOOP"
-*Starts Roleplaying*
-Howl 1
-*Stops Roleplaying Because Of Input*
-Howl "STOP"
-QwQ
-'''
-
-compiler = EsoFurCompiler()
-compiler.compile(code)
+        raise _castingFail()
+        #print(f"Error: Invalid type: {type_name}")
+    
+    
+with open("program.txt") as file: # Use file to refer to the file object
+    code = file.read()
+    print(code)
+    print('---------------------')
+    compiler = EsoFurCompiler()
+    compiler.compile(code)
